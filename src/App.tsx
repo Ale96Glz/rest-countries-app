134bc0c4641cb8cb091d35e7d0f3ed94e579a55b
Alejandro González Osorio <aosorio961020@gmail.com>
2026-08-14 21:39:37 -0600

    Initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header>
+          <h1>Rest Countries</h1>
+        </header>
+        <main>
+          <h1>Countries</h1>
+        </main>
+        <footer>
+          <p>&copy; 2026 Rest Countries. All rights reserved.</p>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default App
